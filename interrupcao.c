cd02366f24f65bd44996e144e5cb9eb729cbe761
yuriccosta <yuricoutinhoc@gmail.com>
2025-01-31 11:15:12 -0300

    Adicionado interrupcao com debouncing

--- interrupcao.c
+++ interrupcao.c
@@ -6,13 +6,18 @@
 
 #include "animacao_matriz.pio.h" // Biblioteca PIO para controle de LEDs WS2818B
 
+// Definição de constantes
 #define LED_PIN_RED 13
 #define LED_COUNT 25            // Número de LEDs na matriz
-#define MATRIZ_PIN 7               // Pino GPIO conectado aos LEDs WS2818B
-#define BUTTON_PIN_A 5               // Pino GPIO conectado ao botão A
-#define BUTTON_PIN_B 6               // Pino GPIO conectado ao botão B
+#define MATRIZ_PIN 7            // Pino GPIO conectado aos LEDs WS2818B
+#define BUTTON_PIN_A 5          // Pino GPIO conectado ao botão A
+#define BUTTON_PIN_B 6          // Pino GPIO conectado ao botão B
 
-volatile int num = 0; // Variável para contar o número de interrupções
+// Declaração de variáveis globais
+PIO pio;
+uint sm;
+volatile uint num = 0; // Variável para contar o número de interrupções
+volatile uint32_t last_time = 0; // Variável para armazenar o tempo do último evento
 
 double padrao[10][LED_COUNT] = {
         {0, 1, 1, 1, 0,
@@ -84,15 +89,15 @@ int ordem[] = {0, 1, 2, 3, 4, 9, 8, 7, 6, 5, 10, 11, 12, 13, 14, 19, 18, 17, 16,
 //rotina para definição da intensidade de cores do led
 uint32_t matrix_rgb(double b, double r, double g)
 {
-  // Para não ficar forte demais, a intensidade de cada cor é multiplicada por 100
+  // Para não ficar forte demais, a intensidade de cada cor é multiplicada por 50
   unsigned char R, G, B;
-  R = r * 100; 
-  G = g * 100;
-  B = b * 100;
+  R = r * 50; 
+  G = g * 50;
+  B = b * 50;
   return (G << 24) | (R << 16) | (B << 8);
 }
 
-void display_num(int number, PIO pio, uint sm){
+void display_num(int number){
     uint32_t valor_led;
 
     for (int i = 0; i < LED_COUNT; i++){
@@ -101,9 +106,30 @@ void display_num(int number, PIO pio, uint sm){
     }
 }
 
+void gpio_irq_handler(uint gpio, uint32_t events) {
+     // Obtém o tempo atual em microssegundos
+    uint32_t current_time = to_ms_since_boot(get_absolute_time());
+    printf("num = %u\n", num);
+    // Verificação de tempo para debounce
+    if (current_time - last_time > 200){
+        if(gpio == BUTTON_PIN_A){
+            printf("Botão A pressionado\n");
+            num++;
+        } else if(gpio == BUTTON_PIN_B){
+            printf("Botão B pressionado\n");
+            num--;
+        }
+        printf("num = %u\n", num);
+
+        display_num(num % 10);
+
+        last_time = current_time; // Atualiza o tempo do último evento
+    }
+}
+
 int main()
 {
-    PIO pio = pio0; 
+    pio = pio0; 
     bool ok;
     
     //coloca a frequência de clock para 128 MHz, facilitando a divisão pelo clock
@@ -116,13 +142,14 @@ int main()
 
     //configurações da PIO
     uint offset = pio_add_program(pio, &animacao_matriz_program);
-    uint sm = pio_claim_unused_sm(pio, true);
+    sm = pio_claim_unused_sm(pio, true);
     animacao_matriz_program_init(pio, sm, offset, MATRIZ_PIN);
 
 
     // Configuração do LED RGB
     gpio_init(LED_PIN_RED);
     gpio_set_dir(LED_PIN_RED, GPIO_OUT);
+    gpio_put(LED_PIN_RED, 0);
 
 
     // Configura os botões
@@ -135,11 +162,16 @@ int main()
     gpio_pull_up(BUTTON_PIN_B);
 
 
+    // Configuração da interrupção
+    gpio_set_irq_enabled_with_callback(BUTTON_PIN_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    gpio_set_irq_enabled_with_callback(BUTTON_PIN_B, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+
+    // Começa a matriz com o número 0
+    display_num(0);
+
     while (true) {
-        for (int i = 0; i < 10; i++){
-            display_num(i, pio, sm);
-            sleep_ms(1000);
-        }
-        sleep_ms(1000);
+        // Pisca o led 5 vezes por segundo
+        gpio_put(LED_PIN_RED, !gpio_get(LED_PIN_RED));
+        sleep_ms(200);
     }
 }
