4b3a9e2104d715d9640302b1cd00d891f4c9f59e
yuriccosta <yuricoutinhoc@gmail.com>
2025-01-31 10:47:18 -0300

    Adicionado animação dos números

--- interrupcao.c
+++ interrupcao.c
@@ -14,14 +14,91 @@
 
 volatile int num = 0; // Variável para contar o número de interrupções
 
+double padrao[10][LED_COUNT] = {
+        {0, 1, 1, 1, 0,
+         0, 1, 0, 1, 0,
+         0, 1, 0, 1, 0,
+         0, 1, 0, 1, 0,
+         0, 1, 1, 1, 0,
+        }, // 0
+        {0, 0, 1, 0, 0,
+         0, 1, 1, 0, 0,
+         1, 0, 1, 0, 0,
+         0, 0, 1, 0, 0,
+         0, 0, 1, 0, 0,
+        }, // 1
+        {0, 1, 1, 1, 0,
+         0, 0, 0, 1, 0,
+         0, 0, 1, 0, 0,
+         0, 1, 0, 0, 0,
+         0, 1, 1, 1, 0,
+        },  // 2
+        {0, 1, 1, 1, 0,
+         0, 0, 0, 1, 0,
+         0, 1, 1, 1, 0,
+         0, 0, 0, 1, 0,
+         0, 1, 1, 1, 0,
+        }, // 3
+        {0, 1, 0, 1, 0,
+         0, 1, 0, 1, 0,
+         0, 1, 1, 1, 0,
+         0, 0, 0, 1, 0,
+         0, 0, 0, 1, 0,
+        }, // 4
+        {0, 1, 1, 1, 0,
+         0, 1, 0, 0, 0,
+         0, 1, 1, 1, 0,
+         0, 0, 0, 1, 0,
+         0, 1, 1, 1, 0,
+        }, // 5
+        {0, 1, 1, 1, 0,
+         0, 1, 0, 0, 0,
+         0, 1, 1, 1, 0,
+         0, 1, 0, 1, 0,
+         0, 1, 1, 1, 0,
+        },  // 6
+        {0, 1, 1, 1, 0,
+         0, 0, 0, 1, 0,
+         0, 0, 1, 0, 0,
+         0, 0, 1, 0, 0,
+         0, 0, 1, 0, 0,
+        }, // 7
+        {0, 1, 1, 1, 0,
+         0, 1, 0, 1, 0,
+         0, 1, 1, 1, 0,
+         0, 1, 0, 1, 0,
+         0, 1, 1, 1, 0,
+        }, // 8
+        {0, 1, 1, 1, 0,
+         0, 1, 0, 1, 0,
+         0, 1, 1, 1, 0,
+         0, 0, 0, 1, 0,
+         0, 1, 1, 1, 0,
+        }  // 9
+    };
+
+// Ordem da matriz de LEDS
+int ordem[] = {0, 1, 2, 3, 4, 9, 8, 7, 6, 5, 10, 11, 12, 13, 14, 19, 18, 17, 16, 15, 20, 21, 22, 23, 24};  
+
+
 //rotina para definição da intensidade de cores do led
 uint32_t matrix_rgb(double b, double r, double g)
 {
+  // Para não ficar forte demais, a intensidade de cada cor é multiplicada por 100
   unsigned char R, G, B;
-  R = r * 255;
-  G = g * 255;
-  B = b * 255;
+  R = r * 100; 
+  G = g * 100;
+  B = b * 100;
   return (G << 24) | (R << 16) | (B << 8);
+}
+
+void display_num(int number, PIO pio, uint sm){
+    uint32_t valor_led;
+
+    for (int i = 0; i < LED_COUNT; i++){
+        valor_led = matrix_rgb(0, padrao[number][ordem[24 - i]], 0);
+        pio_sm_put_blocking(pio, sm, valor_led);
+    }
 }
 
 int main()
@@ -59,7 +136,10 @@ int main()
 
 
     while (true) {
-        printf("Hello, world!\n");
+        for (int i = 0; i < 10; i++){
+            display_num(i, pio, sm);
+            sleep_ms(1000);
+        }
         sleep_ms(1000);
     }
 }
